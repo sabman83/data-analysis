35bd378f69b0945514816de9eecc2397c5b5d0e1
Sebastin Kolman <skolman@rocketfuel.com>
2016-11-09 16:54:12 -0800

    First Commit

---
+++ conversion-rate/analysis/init.R
@@ -0,0 +1,24 @@
+if (!require("pacman")) install.packages("pacman")
+pacman::p_load(data.table, ggplot2)
+
+
+setwd("./conversion-rate/")
+conversion_rate_data <- read.csv("data/conversion_data.csv")
+conversion_rate_table <- data.table(conversion_rate_data)
+conversion_rate_table$new_user <- as.factor(conversion_rate_table$new_user)
+conversion_rate_table$converted <- as.factor(conversion_rate_table$converted)
+summary(conversion_rate_table)
+
+
+ggplot(conversion_rate_table, aes(x = age, y = total_pages_visited)) 
+      + geom_point(aes(color = factor(converted))) 
+      + geom_vline(xintercept = 61, color = "red")
+
+ggplot(converted_users, aes(x = age, y = source)) 
+      + geom_point(aes(color = total_pages_visited)) 
+
+ggplot(converted_users, aes(x = age, y = source)) 
+      + geom_point(aes(color = factor(new_user))) 
+
+ggplot(conversion_rate_table, aes(x = age, y = country)) + geom_point(aes(color = factor(converted)))
+ggplot(converted_users, aes(x = age, y = country)) + geom_point(aes(color = factor(converted)))
---
+++ conversion-rate/notes/initial_findings.md
@@ -0,0 +1,26 @@
+*On Summarizing*
+
+- Only 4 countries (China, Germnay, UK and US)
+- Mean age of 30.57, range from 17 - 123, 123 seems strange, maybe an error?
+- Mean session of 4.8 pages
+- Percentage of new users  = 68.55
+- Percentage of converted users = 3.225
+- No NA or omitted data
+
+- plotting age vs converted doesn't seem to imply anything
+-  there seems to be 2 outliers
+- no one from china converted
+- All those who visit more than 20 definitely convert
+- Ages 61 and over don't convert
+- Sources doesn't seem to play a pivotal role
+- Amongst the converted users most are not new users
+- Fewer converted users are from China
+
+- Current conversion  = conversion / total sessions = 0.66198 %
+
+
+?-  So we need to predict conversion rate. Isn't it enough to predict a conversion?
+
+*Analysis*
+
+*Recommendations*
